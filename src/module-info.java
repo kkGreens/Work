3aed58ece0264774f42ece1fed9dc1f782f40ec0
kk <karkuzhaliomrbranch@gmail.com>
2025-04-18 14:24:19 +0530

    initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SamplePull {
+}
---
+++ src/org/com/First.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class First {
+	
+	
+	public void m1() {
+		System.out.println("Welcome");
+	}
+
+	public static void main(String[] args) {
+		
+		First task = new First();
+		task.m1();
+	}
+}
