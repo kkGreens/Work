cf9b0c7d920af8dc04d4e8de3cd3696dfa7aad1e
kk <karkuzhaliomrbranch@gmail.com>
2025-04-18 14:37:28 +0530

    message

--- src/org/com/Second.java
+++ src/org/com/Second.java
@@ -4,11 +4,16 @@ public class Second {
 	public void m1() {
 	System.out.println("Selenium");
 }
+	
+	public void m2() {
+		System.out.println("Java");
+	}
 
 public static void main(String[] args) {
 	
 	Second task = new Second();
 	task.m1();
+	task.m2();
 }
 
 }
